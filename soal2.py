62e1c53d251015179b7c91dfd9056f3a9a579d13
Muhammad Yusuf <abstraker@gmail.com>
2017-09-04 09:22:14 -0700

    4 Sept

--- soal2.py
+++ soal2.py
@@ -12,3 +12,52 @@ inputan = input("Masukkan: ")
 
 # FUNGSI VALIDASI BRACKETS
 # Tulis algoritma disini...
+class Stack:
+	def __init__(self):
+		self.items = []
+		
+	def isEmpty(self):
+		return self.items == []
+	def push(self, elm):
+		return self.items.append(elm)
+	def pop(self):
+		if not self.isEmpty():
+			return self.items.pop()
+	def size(self):
+		return len(self.items)
+		
+	def printStack(self):
+		for i in self.items:
+			print(i, end="")
+		print()
+
+#MAIN PROGRAM	
+cek = True
+buka = ["(", "{", "["]
+tutup = [")", "}", "]"]
+data = Stack()
+i = 0
+while (i< len(inputan)) and cek:
+	if inputan[i] in buka: 
+		data.push(inputan[i])
+
+	elif inputan[i] in tutup:
+		if data.isEmpty():
+			cek = False
+			break
+		else:
+			temp = data.pop()
+			if inputan[i] == ")" and temp == "(":	
+				cek = True
+			elif inputan[i] == "}" and temp == "{":	
+				cek = True
+			elif inputan[i] == "]" and temp == "[":	
+				cek = True
+			else: cek = False
+		
+	i +=1		
+
+if cek and data.isEmpty():
+	print("Seimbang ")
+else:
+	print("Tidak Seimbang")
