4d5b2a7e8a895420c37279d714c0fe9d1af2f05e
Maxalmina <maxalmina.kahfi@gmail.com>
2017-09-07 06:08:34 +0700

    soal 1 2 3

--- soal1.py
+++ soal1.py
@@ -7,3 +7,14 @@ arr = [randint(1,100) for i in range(0,100)]
 
 # FUNGSI INSERTION SORT
 # Tulis algoritma disini...
+print(arr)
+pass1 = 0
+for pass1 in range(len(arr)-1):
+	i = pass1 + 1
+	temp = arr[i]
+	while temp<arr[i-1] and i>0:
+		arr[i] = arr[i-1]
+		i = i-1
+	arr[i] = temp
+
+print("\nHasil Insertion Sort(Ascending) : ",arr)
