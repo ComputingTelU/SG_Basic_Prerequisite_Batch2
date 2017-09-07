0cb946beaa7934527dba640cde69279dc25d2da2
Wisnu Adi Nurcahyo <nurcahyo@protonmail.com>
2017-09-07 20:15:04 +0700

    Add answer for soal1 and soal2
    Too lazy to answer the rest

--- soal1.py
+++ soal1.py
@@ -7,3 +7,38 @@ arr = [randint(1,100) for i in range(0,100)]
 
 # FUNGSI INSERTION SORT
 # Tulis algoritma disini...
+print("Before:")
+print(arr)
+print()
+
+class Sort:
+    numbers = []
+
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+    def get(self):
+        return self.numbers
+
+    def insertion(self):
+        numbers  = self.numbers
+        length   = len(numbers)
+
+        for i in range(1, length):
+            cur = numbers[i]
+            pos = i
+
+            while pos > 0 and numbers[pos - 1] > cur:
+                numbers[pos] = numbers[pos - 1]
+
+                pos -= 1
+
+            numbers[pos] = cur
+
+sort = Sort(arr)
+sort.insertion()
+
+result = sort.get()
+
+print("Result:")
+print(result)
