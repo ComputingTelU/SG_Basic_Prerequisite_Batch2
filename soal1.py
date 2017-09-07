348740a4477abcd9b2a26dbdfdb03ffdee49220a
wikankun <wikan25@gmail.com>
2017-09-07 12:18:32 +0700

    Jawaban Wikan

--- soal1.py
+++ soal1.py
@@ -7,3 +7,16 @@ arr = [randint(1,100) for i in range(0,100)]
 
 # FUNGSI INSERTION SORT
 # Tulis algoritma disini...
+def insertion_sort(array):
+	for i in range(1, len(array)):
+		nilai = array[i]
+		j = i - 1
+		while (j>=0) and (array[j]>nilai):
+			array[j+1] = array[j]
+			j = j - 1
+		array[j+1] = nilai
+
+insertion_sort(arr)
+for x in arr:
+	print(x)
+	
--- soal2.py
+++ soal2.py
@@ -9,6 +9,36 @@ inputan = input("Masukkan: ")
 # pop() -> mengeluarkan item pada Stack
 # size() -> menghitung jumlah item pada Stack
 # printStack() -> output seluruh isi Stack
+class Stack:
+	def __init__(self):
+		self.items = []
+	def isEmpty(self):
+		return self.items == []
+	def push(self, item):
+		self.items.append(item)
+	def pop(self):
+		return self.items.pop()
+	def size(self):
+		return len(self.items)
+	def printStack(self):
+		return self.items[len(self.items)-1]
+
 
 # FUNGSI VALIDASI BRACKETS
 # Tulis algoritma disini...
+def validation(inputan):
+	stat = True
+	s = Stack()
+	for items in inputan:
+		if items == '(':
+			s.push(items)
+		elif items == ')':
+			if not s.isEmpty():
+				s.pop()
+			else:
+				return False
+	if not s.isEmpty():
+		return False
+	return stat
+
+print(validation(inputan))
