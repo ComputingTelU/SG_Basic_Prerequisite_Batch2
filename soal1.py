381571ce22dde0607b1eff0e786f30fa858409e4
hermawanwiwid <hermawanwiwid@gmail.com>
2017-09-08 07:00:16 +0700

    WIWIT
    hmmm

--- soal1.py
+++ soal1.py
@@ -3,7 +3,15 @@ from random import randint
 
 # VARIABLE
 # Berisi angka secara random dari 0 - 100 sebanyak 100 angka
-arr = [randint(1,100) for i in range(0,100)]
+array = [randint(1,100) for i in range(0,100)]
 
-# FUNGSI INSERTION SORT
-# Tulis algoritma disini...
+def ins_sort(k):
+    for i in range(1,len(k)):   
+        j = i                   
+        while j > 0 and k[j] < k[j-1]: 
+            k[j], k[j-1] = k[j-1], k[j]
+            j=j-1 
+    return k
+
+ins_sort(array)
+print(array)
--- soal2.py
+++ soal2.py
@@ -1,14 +1,34 @@
-# VARIABLE
-# Sebagai inputan yang akan divalidasi
-inputan = input("Masukkan: ")
+class Stack:
 
-# KELAS STACK
-# Buatlah kelas objek Stack dengan 1 atribut yaitu array, lalu memiliki fungsi sbg berikut:
-# isEmpty() -> return True jika Stack kosong dan False jika terisi.
-# push(item) -> memasukkan item kedalam Stack
-# pop() -> mengeluarkan item pada Stack
-# size() -> menghitung jumlah item pada Stack
-# printStack() -> output seluruh isi Stack
+     def __init__(self):
+          self.items = []  
+     def isEmpty(self):
+          return self.items == []  
+     def push(self, item):
+          self.items.append(item)  
+     def pop(self):
+          return self.items.pop()  
+     def peek(self):
+          return self.items[len(self.items)-1]  
+     def size(self):
+          return len(self.items)
 
-# FUNGSI VALIDASI BRACKETS
-# Tulis algoritma disini...
+s = Stack()
+
+inputan = "))(("
+
+for x in inputan:
+     if (x == "("):
+          s.push(x)
+     elif (x == ")"):
+          # s.pop()
+          if (s.isEmpty()):
+                cek = False
+          else:
+               s.pop()
+
+if (s.isEmpty() == True) and (cek == True):
+     print(inputan, " VALID BGT IH")
+elif (s.isEmpty() == False) or (cek == False):
+     print(inputan, " NOT VALID BOSS")
+
